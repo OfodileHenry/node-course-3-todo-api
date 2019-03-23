a62ed892851443d5a62f52a4e4146ac6c100e968
Henry <ofodilechukwuka@gmail.com>
2019-03-23 09:41:41 +0100

    ADD DELETE /todos/:id route

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,7 +1,7 @@
-const {Object ID} = require("mongodb");
+const {ObjectID} = require("mongodb");
 const {mongoose} = require("./../server/db/mongoose");
 const {Todo} = require("./../server/models/todo");
-const (User) = require("./../server/models/user");
+const {User} = require("./../server/models/user");
 var id = "5c91f8e123403ea83126916f"
 
 Todo.find({
--- server/server.js
+++ server/server.js
@@ -44,8 +44,24 @@ app.get("/todos/:id",(req,res)=>{
     res.send({todo});
   }).catch((e)=>{
     res.status(400).send();
-  })
+  });
   // res.send(req.params);
+});
+
+app.delete("/todos/:id",(req,res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo)=>{
+     if(!todo){
+       res.status(404).send()
+     }
+     res.send(todo)
+  }).catch((e)=>{
+    res.status(400).send();
+  })
 })
 app.listen(port,()=>{
   console.log(`The app is up and running!${port}`)
